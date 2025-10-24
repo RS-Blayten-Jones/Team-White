4049858ee6e43f0b2c8a336671417137da74a018
Christy Dunlap <cdunlap@rocketsoftware.com>
2025-10-24 15:47:47 -0500

    update functions

---
+++ all_the_buzz/database.py
@@ -0,0 +1,15 @@
+# content is {"joke": "", "difficulty": ..}
+def add(table, content, person):
+    # check status of person
+    # add to table 
+    # return 
+
+def delete(table, id, person ):
+    # check status of person
+    # delete record from table
+
+def retrievedata(table, query, person):
+    # return json of records{{}}
+
+def retrieveRecord(table, id,person):
+    # return one record
