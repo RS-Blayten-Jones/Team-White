21d4ef200bf92643b85ffbb11e4fa9f006cca08f
Kassidy Wall <kwall@rocketsoftware.com>
2025-10-29 13:59:16 -0500

    deleted my own database schema folder and started working on server.py

--- all_the_buzz/server.py
+++ all_the_buzz/server.py
@@ -1,1 +1,13 @@
-# house all entity classes
+from flask import Flask, request
+
+
+class MyFlask(Flask):
+    def add_url_rule(self, rule, endpoint=None, view_func=None, **options):
+        return super().add_url_rule(rule, endpoint, view_func, provide_automatic_options=False, **options)
+
+app = MyFlask(__name__)
+#CORS Preflight necessary only when we integrate with front end 
+
+@ app.route("/jokes", methods=["GET"])
+def retrieve_public_jokes_collection():
+    pass
