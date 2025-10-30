8b22ef11357c9bc5ea201dc839a8305f5ba395b1
RS-Blayten-Jones <bjones2@rocketsoftware.com>
2025-10-30 15:31:20 -0500

    Added documentation and other neat DAO features!

--- all_the_buzz/database_operations/jokes_dao.py
+++ all_the_buzz/database_operations/jokes_dao.py
@@ -1,6 +1,13 @@
 from abstract_record import DatabaseAccessObject
 
 class PublicJokeDAO(DatabaseAccessObject):
+    ROLE_MATRIX = {
+        "read": ["Employee", "Manager"],
+        "create": ["Manager"],
+        "update": ["Manager"],
+        "delete": ["Manager"]
+    }
+
     def __init__(self, client_uri: str, database_name: str):
         super().__init__("jokes_public", client_uri, database_name)
 
--- all_the_buzz/database_operations/trivia_dao.py
+++ all_the_buzz/database_operations/trivia_dao.py
@@ -1,9 +1,23 @@
 from abstract_record import DatabaseAccessObject
 
 class PublicTriviaDAO(DatabaseAccessObject):
+    ROLE_MATRIX = {
+        "read": ["Employee", "Manager"],
+        "create": ["Manager"],
+        "update": ["Manager"],
+        "delete": ["Manager"]
+    }
+
     def __init__(self, client_uri: str, database_name: str):
         super().__init__("trivia_public", client_uri, database_name)
 
 class PrivateTriviaDAO(DatabaseAccessObject):
+    ROLE_MATRIX = {
+        "read": ["Manager"],
+        "create": ["Employee", "Manager"],
+        "update": ["Manager"],
+        "delete": ["Manager"]
+    }
+
     def __init__(self, client_uri: str, database_name: str):
         super().__init__("trivia_private", client_uri, database_name)
