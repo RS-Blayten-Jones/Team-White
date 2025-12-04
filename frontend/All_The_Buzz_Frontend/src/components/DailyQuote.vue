d411c6404231227f0d1ef6efa890237ee566f016
rocketchristy <cdunlap@rocketsoftware.com>
2025-12-04 16:32:53 -0500

    tested daily quotes

--- frontend/All_The_Buzz_Frontend/src/components/DailyQuote.vue
+++ frontend/All_The_Buzz_Frontend/src/components/DailyQuote.vue
@@ -27,7 +27,7 @@ export default defineComponent({
 	mounted() {
 		console.log("token", this.jwt);
 		this.getDailyQuote();
-		const images = ['img1.jpg', 'img2.jpg', 'img3.jpg']; // Add your filenames here
+		const images = ['img1.jpg']; // Add your filenames here
   		const randomImg = images[Math.floor(Math.random() * images.length)];
   		this.bgImage = `/dailyquote/${randomImg}`;
 	},
@@ -51,16 +51,27 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.daily-quote blockquote, .daily-quote cite {
+  -webkit-text-stroke: 1px white;
+}
+.daily-quote {
+  aspect-ratio: 1/1; /* modern browsers */
+  background-size: cover;
+  background-position: center;
+  overflow: hidden;
+}
 .daily-quote {
 	background-size: cover;
   	background-position: center;
 	flex:1;
 	text-align: center;
-	font-size: 1.25rem;
+	font-size: 3rem;
 	color: var(--text-primary, #222);
 	margin: 2rem 0;
 	background: #d1c1e9ff;
 	padding: 1.5rem 2rem;
+	font-weight: bold;
+	
 }
 
 blockquote {
@@ -70,6 +81,6 @@ blockquote {
 cite {
 	display: block;
 	color: var(--text-secondary, #666);
-	font-size: 1rem;
+	font-size: 1.7rem;
 }
 </style>
