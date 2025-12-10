690e0e82cf3cb7d2882fe4ddab6839144ddcb692
RS-Blayten-Jones <noone@nowhere.com>
2025-12-10 12:56:15 -0500

    squashing errors

--- frontend/All_The_Buzz_Frontend/src/views/Login.vue
+++ frontend/All_The_Buzz_Frontend/src/views/Login.vue
@@ -43,7 +43,7 @@ async function handleLogin(credentials: { username: string; password: string }) 
     // const token = loginRes.data.token
 
     // For now, use hardcoded token as requested:
-    const hardcodedJwt = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRoIFNlcnZpY2UiLCJsYXN0X25hbWUiOiJGaXNoYmllIiwibG9jYXRpb24iOiJKYXBhbiIsImlkIjo2NTgsImRlcGFydG1lbnQiOiJMZWdhbCIsInRpdGxlIjoiTWFuYWdlciIsImZpcnN0X25hbWUiOiJKb2xlZW4iLCJzdWIiOiJKb2xlZW4gRmlzaGJpZSIsImlhdCI6MTc2NTM4NjcxNywiZXhwIjoxNzY1MzkwMzE3fQ.Iw9UrGxRkYXcTRutNw5AVucrRQnob-uI8Kk4Bu1z3KE' //PUT TOKEN HERE!
+    const hardcodedJwt = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRoIFNlcnZpY2UiLCJsYXN0X25hbWUiOiJGaXNoYmllIiwibG9jYXRpb24iOiJKYXBhbiIsImlkIjo2NTgsImRlcGFydG1lbnQiOiJMZWdhbCIsInRpdGxlIjoiTWFuYWdlciIsImZpcnN0X25hbWUiOiJKb2xlZW4iLCJzdWIiOiJKb2xlZW4gRmlzaGJpZSIsImlhdCI6MTc2NTM4OTA5MiwiZXhwIjoxNzY1MzkyNjkyfQ.ynA-6yO9K4WDqZ-EvHv-yaPMgcLmm2j5jocLNO7q6H8' //PUT TOKEN HERE!
     const token = hardcodedJwt
 
     // --- Step 2: Exchange token for Credentials using your auth server ---
